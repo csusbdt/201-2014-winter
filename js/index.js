40538f754131b500f6d0ec4c7da960cfd00b7226
David Turner <csusbdt@gmail.com>
2013-12-08 20:46:05 -0800

    X

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 schedule = [
-  ['L1', 20, 'The Linux Command Line',  new Date(2014,  1, 10)],
-  ['L2', 30, 'Expressions',             new Date(2014,  1, 14)],
-  ['A1', 50, 'Analyzing Code',          new Date(2014,  1, 19)],
-  ['L3', 30, 'Another Lab',             new Date(2014,  1, 23)],
-  ['L4', 30, 'Expressions Lab 2',       new Date(2014,  1, 28)],
-  ['Q1', 50, 'Quiz 1',                  new Date(2014,  2,  3)],
+  ['L1',  20, new Date(2014,  1, 10), '<a href="labs/linux.html">L1: The Linux Command Line</a>'],
+  ['L2',  30, new Date(2014,  1, 14), '<a href="labs/expressions.html">L2: Expressions</a>'],
+  ['A1',  50, new Date(2014,  1, 19), '<a href="assignments/analyze.html">A1: Analyzing Code</a>'],
+  ['Q1', 200, new Date(2014,  1, 23), 'Quiz 1'],
+  ['L3',  40, new Date(2014,  1, 28), '<a href="labs/algorithms.html">L3: Algorithms</a>'],
+  ['L4',  30, new Date(2014,  2,  3), '<a href="labs/functions.html">L4: Functions</a>'],
+  ['L5',  20, new Date(2014,  2,  3), '<a href="labs/classes.html">L5: Classes</a>'],
+  ['A2',  50, new Date(2014,  1, 19), '<a href="assignments/classes.html">A2: Classes</a>'],
+  ['Q2', 200, new Date(2014,  1, 23), 'Quiz 2'],
+  ['L6',  20, new Date(2014,  2,  3), '<a href="labs/arrays.html">L6: Arrays</a>'],
+  ['A3',  50, new Date(2014,  1, 19), '<a href="assignments/linear.html">A3: Linear Algorithms</a>'],
+  ['L7',  20, new Date(2014,  2,  3), '<a href="labs/search.html">L7: Binary Search</a>'],
+  ['A4',  40, new Date(2014,  1, 19), '<a href="assignments/linear2.html">A4: More Linear Algorithms</a>'],
+  ['Q3', 200, new Date(2014,  1, 23), 'Quiz 3'],
+  ['L8',  20, new Date(2014,  2,  3), '<a href="labs/sorting.html">L7: Sorting</a>'],
+  ['F' , 400, new Date(2014,  1, 23), 'Final Exam']
 ];
 
 (function() {
@@ -43,7 +53,7 @@ app.loadScript = function(url) {
 };
 
 app.onload = function() {
-  app.buildPage();
+  app.buildTable();
   var id = app.getUrlRequestParams()['id'];
   if (id) {
     app.studentId = id;
@@ -51,19 +61,19 @@ app.onload = function() {
   }
 };
 
-app.buildPage = function() {
+app.buildTable = function() {
   var tbody = document.getElementsByTagName('tbody')[0];
   for (var i = 0; i < schedule.length; ++i) {
     var tr = document.createElement('tr');
-    var description = document.createElement('td');
-    description.innerHTML = schedule[i][2];
-    tr.appendChild(description);
-    var deadline = document.createElement('td');
-    deadline.innerHTML = app.extractDate(schedule[i][3]);
-    tr.appendChild(deadline);
-    points = document.createElement('td');
-    points.innerHTML = schedule[i][1];
-    tr.appendChild(points);
+    var td = document.createElement('td'); // activity column
+    td.innerHTML = schedule[i][3];
+    tr.appendChild(td);
+    td = document.createElement('td'); // deadline
+    td.innerHTML = app.extractDate(schedule[i][2]);
+    tr.appendChild(td);
+    td = document.createElement('td'); // points
+    td.innerHTML = schedule[i][1];
+    tr.appendChild(td);
     tbody.appendChild(tr);
   }
 };
