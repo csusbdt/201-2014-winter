26df3c8b3e4cfb4733a0434ec842b65d544887e9
David Turner <csusbdt@gmail.com>
2013-12-09 23:47:07 -0800

    X

--- js/index.js
+++ js/index.js
@@ -2,20 +2,20 @@
 
   schedule = [
     ['L1',  20, new Date(2014,  1, 10), '<a href="L1.html">L1: Command Line</a>'],
-    ['L2',  30, new Date(2014,  1, 14), '<a href="labs/expressions.html">L2: Expressions</a>'],
-    ['A1',  50, new Date(2014,  1, 19), '<a href="assignments/analyze.html">A1: Analyzing Code</a>'],
+    ['L2',  30, new Date(2014,  1, 14), '<a href="L2.html">L2: Expressions</a>'],
+    ['A1',  50, new Date(2014,  1, 19), '<a href="A1.pdf">A1: Analyzing Code</a>'],
     ['Q1', 200, new Date(2014,  1, 23), 'Quiz 1'],
-    ['L3',  40, new Date(2014,  1, 28), '<a href="labs/algorithms.html">L3: Algorithms</a>'],
-    ['L4',  30, new Date(2014,  2,  3), '<a href="labs/functions.html">L4: Functions</a>'],
-    ['L5',  20, new Date(2014,  2,  3), '<a href="labs/classes.html">L5: Classes</a>'],
-    ['A2',  50, new Date(2014,  1, 19), '<a href="assignments/classes.html">A2: Classes</a>'],
+    ['L3',  40, new Date(2014,  1, 28), '<a href="L3.html">L3: Algorithms</a>'],
+    ['L4',  30, new Date(2014,  2,  3), '<a href="L4.html">L4: Functions</a>'],
+    ['L5',  20, new Date(2014,  2,  3), '<a href="L5.html">L5: Classes</a>'],
+    ['A2',  50, new Date(2014,  1, 19), '<a href="A2.pdf">A2: Classes</a>'],
     ['Q2', 200, new Date(2014,  1, 23), 'Quiz 2'],
-    ['L6',  20, new Date(2014,  2,  3), '<a href="labs/arrays.html">L6: Arrays</a>'],
-    ['A3',  50, new Date(2014,  1, 19), '<a href="assignments/linear.html">A3: Linear Algorithms</a>'],
-    ['L7',  20, new Date(2014,  2,  3), '<a href="labs/search.html">L7: Binary Search</a>'],
-    ['A4',  40, new Date(2014,  1, 19), '<a href="assignments/linear2.html">A4: More Linear Algorithms</a>'],
+    ['L6',  20, new Date(2014,  2,  3), '<a href="L6.html">L6: Arrays</a>'],
+    ['A3',  50, new Date(2014,  1, 19), '<a href="A3.pdf">A3: Linear Algorithms</a>'],
+    ['L7',  20, new Date(2014,  2,  3), '<a href="L7.html">L7: Binary Search</a>'],
+    ['A4',  40, new Date(2014,  1, 19), '<a href="A4.pdf">A4: More Algorithms</a>'],
     ['Q3', 200, new Date(2014,  1, 23), 'Quiz 3'],
-    ['L8',  20, new Date(2014,  2,  3), '<a href="labs/sorting.html">L7: Sorting</a>'],
+    ['L8',  20, new Date(2014,  2,  3), '<a href="L8.html">L7: Sorting</a>'],
     ['F' , 400, new Date(2014,  1, 23), 'Final Exam']
   ];
 
