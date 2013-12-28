31e7c0332f80099731db5dd98031b961b8f616e7
Gerren <gdwillis@gmail.com>
2013-12-27 17:40:02 -0800

    x

--- scores.js
+++ scores.js
@@ -1,5 +1,5 @@
 app.scores = {};//  L1    L2    A1    Q1    L3    L4    L5    A2    Q2    L6    A3    L7    A4    Q3    L7     F
-app.scores.dt = [   10,   20,   25,   30, null, null, null, null, null, null, null, null, null, null, null, null ];
+app.scores.dt = [   10,   20,   25,   30,   13, null, null, null, null, null, null, null, null, null, null, null ];
 app.scores.xx = [   10,   20, null,   50, null, null, null, null, null, null, null, null, null, null, null, null ];
 app.scores.yy = [   10,   20,   10, null, null, null, null, null, null, null, null, null, null, null, null, null ];
 app.scores.zz = [   10,   20,   10,    6, null, null, null, null, null, null, null, null, null, null, null, null ];
