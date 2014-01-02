e9f4b293386111cd01e1e1b4db5b2fa6633ff7a4
David Turner <csusbdt@gmail.com>
2014-01-01 20:27:10 -0800

    X

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
 (function() {
 
+  var jan = 0, feb = 1, mar = 3;
+
   schedule = [
-    ['L1',  20, new Date(2014,  1, 10), '<a href="L1.html">L1: Command Line</a>'],
-    ['L2',  30, new Date(2014,  1, 14), '<a href="L2.html">L2: Expressions</a>'],
-    ['A1',  50, new Date(2014,  1, 19), '<a href="A1.pdf">A1: Analyzing Code</a>'],
-    ['Q1', 200, new Date(2014,  1, 23), 'Quiz 1'],
-    ['L3',  40, new Date(2014,  1, 28), '<a href="L3.html">L3: Algorithms</a>'],
-    ['L4',  30, new Date(2014,  2,  3), '<a href="L4.html">L4: Functions</a>'],
-    ['L5',  20, new Date(2014,  2,  3), '<a href="L5.html">L5: Classes</a>'],
-    ['A2',  55, new Date(2014,  1, 19), '<a href="A2.pdf">A2: Classes</a>'],
-    ['Q2', 200, new Date(2014,  1, 23), 'Quiz 2'],
-    ['L6',  30, new Date(2014,  2,  3), '<a href="L6.html">L6: Arrays</a>'],
-    ['L7',  20, new Date(2014,  2,  3), '<a href="L7.html">L7: Binary Search</a>'],
-    ['A3',  50, new Date(2014,  1, 19), '<a href="A3.pdf">A3: Algorithms 2</a>'],
-    ['Q3', 200, new Date(2014,  1, 23), 'Quiz 3'],
-    ['L8',  30, new Date(2014,  2,  3), '<a href="L8.html">L8: Sorting</a>'],
-    ['F' , 400, new Date(2014,  1, 23), 'Final Exam']
+    ['L1',  20, new Date('2014-01-09T23:59:59'), '<a href="L1.html">L1: Command Line</a>'],
+    ['L2',  30, new Date('2014-01-16T23:59:59'), '<a href="L2.html">L2: Expressions</a>'],
+    ['A1',  50, new Date('2014-01-21T23:59:59'), '<a href="A1.pdf">A1: Analyzing Code</a>'],
+    ['Q1', 200, new Date('2014-01-27T23:59:59'), '<a href="Q1.html">Quiz 1</a>'],
+    ['L3',  40, new Date('2014-01-30T23:59:59'), '<a href="L3.html">L3: Algorithms</a>'],
+    ['L4',  30, new Date('2014-02-06T23:59:59'), '<a href="L4.html">L4: Functions</a>'],
+    ['L5',  20, new Date('2014-02-13T23:59:59'), '<a href="L5.html">L5: Classes</a>'],
+    ['A2',  55, new Date('2014-02-16T23:59:59'), '<a href="A2.pdf">A2: Classes</a>'],
+    ['Q2', 200, new Date('2014-02-19T23:59:59'), 'Quiz 2'],
+    ['L6',  30, new Date('2014-02-27T23:59:59'), '<a href="L6.html">L6: Arrays</a>'],
+    ['A3',  50, new Date('2014-03-02T23:59:59'), '<a href="A3.pdf">A3: Algorithms 2</a>'],
+    ['L7',  20, new Date('2014-03-06T23:59:59'), '<a href="L7.html">L7: Binary Search</a>'],
+    ['Q3', 200, new Date('2014-03-10T23:59:59'), 'Quiz 3'],
+    ['L8',  30, new Date('2014-03-17T23:59:59'), '<a href="L8.html">L8: Sorting</a>'],
+    ['F' , 400, new Date('2014-03-19T12:00:00'), 'Final Exam']
   ];
 
   app.onload = function() {
