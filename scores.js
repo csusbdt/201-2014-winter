dd5c13510780d057164e77ade7d64142ad22db30
David Turner <csusbdt@gmail.com>
2014-01-07 16:58:43 -0800

    X

--- scores.js
+++ scores.js
@@ -1,6 +1,6 @@
 app.scores = {};//  L1    L2    A1    Q1    L3    L4    L5    A2    Q2    L6    A3    L7    A4    Q3    L7     F
-app.scores.dt = [ null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null ];
-app.scores.xx = [ null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null ];
+app.scores.ab = [ null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null ];
+app.scores.gh = [ null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null ];
 
 app.onloadScores();
 
